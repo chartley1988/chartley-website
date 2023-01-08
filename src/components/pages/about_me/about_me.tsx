00aa72308159f4bd3f2f325d5c54c874b7858ab8
Carson Hartley <chartley1988@gmail.com>
2023-01-08 16:27:03 -0700

    Added link urls to social links

--- src/components/pages/about_me/about_me.tsx
+++ src/components/pages/about_me/about_me.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './about_me.css';
 import photo from '/Users/chartley/Documents/Projects/chartley-website/src/images/about_me_photo.jpg';
 
-
 function AboutMe() {
 	return (
 		<div className='about-me'>
@@ -11,7 +10,7 @@ function AboutMe() {
 				Calgary area in Alberta, Canada. I have a passion for creating
 				things, sometimes from code, sometimes from wood.
 			</p>
-			<img src={photo}></img>
+			<img src={photo} alt="Photo of Me"></img>
 		</div>
 	);
 }
--- src/components/social_bar/social_bar.tsx
+++ src/components/social_bar/social_bar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Router } from 'react-router';
 import './social_bar.css';
 import twitterLogo from '/Users/chartley/Documents/Projects/chartley-website/src/images/Twitter social icons - circle - blue.svg';
 import instaLogo from '/Users/chartley/Documents/Projects/chartley-website/src/images/Instagram_Glyph_Gradient_RGB.svg';
@@ -6,18 +7,25 @@ import instaLogo from '/Users/chartley/Documents/Projects/chartley-website/src/i
 function SocialBar() {
 	return (
 		<div className='social-bar'>
-			<svg
-				xmlns='http://www.w3.org/2000/svg'
-				viewBox='0 0 24 24'
-				fill='currentColor'
-				className='w-6 h-6'
-			>
-				<path d='M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z' />
-				<path d='M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z' />
-			</svg>
+			<a href='mailto:chartley1988@gmail.com'>
+				<svg
+					xmlns='http://www.w3.org/2000/svg'
+					viewBox='0 0 24 24'
+					fill='currentColor'
+					className='w-6 h-6'
+				>
+					<path d='M1.5 8.67v8.58a3 3 0 003 3h15a3 3 0 003-3V8.67l-8.928 5.493a3 3 0 01-3.144 0L1.5 8.67z' />
+					<path d='M22.5 6.908V6.75a3 3 0 00-3-3h-15a3 3 0 00-3 3v.158l9.714 5.978a1.5 1.5 0 001.572 0L22.5 6.908z' />
+				</svg>
+			</a>
 
-			<img src={twitterLogo}></img>
-			<img src={instaLogo}></img>
+			<a href='https://twitter.com/chartley1988'>
+				<img src={twitterLogo} alt='Twitter Logo'></img>
+			</a>
+
+			<a href='https://www.instagram.com/chartley1988/'>
+				<img src={instaLogo} alt='Instagram Logo'></img>
+			</a>
 		</div>
 	);
 }
