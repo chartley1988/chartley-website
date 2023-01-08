0606c0901039e1105ba969496496eb6ccdd82b1d
Carson Hartley <chartley1988@gmail.com>
2023-01-07 22:22:04 -0700

    Build

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import Header from './components/header/header'
 import NavMenu from './components/nav_menu/nav_menu'
 import "./styles/main.css"
