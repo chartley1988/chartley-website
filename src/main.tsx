9a4a5998f3544061d524de995f8dc39ac67b1d26
Carson Hartley <chartley1988@gmail.com>
2023-01-09 09:39:52 -0700

    Route Fixes

--- src/main.tsx
+++ src/main.tsx
@@ -17,7 +17,7 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '',
+        path: 'chartley-website',
         element: <Home />
       },
       {
