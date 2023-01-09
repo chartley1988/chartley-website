16825b80b0fab6498ead52676051f94c3ac8db4b
Carson Hartley <chartley1988@gmail.com>
2023-01-09 10:16:50 -0700

    Rolled back a few commits.

--- src/main.tsx
+++ src/main.tsx
@@ -17,6 +17,10 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
+        path: '',
+        element: <Home />
+      },
+      {
         path: 'about-me',
         element: <AboutMe />
       },
