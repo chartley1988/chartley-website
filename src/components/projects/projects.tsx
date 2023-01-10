9618d5b3167b4bb4227b0c89f179be89197a7bd5
Carson Hartley <chartley1988@gmail.com>
2023-01-09 20:07:38 -0700

    Progress on projects page

--- src/components/projects/projects.tsx
+++ src/components/projects/projects.tsx
@@ -1,38 +1,44 @@
-import React from "react";
-import './projects.css'
-import projects_data from "/Users/chartley/Documents/Projects/chartley-website/src/data/projects_data.json"
+import React from 'react';
+import './projects.css';
+import { Link } from 'react-router-dom';
+import projects_data from '/Users/chartley/Documents/Projects/chartley-website/src/data/projects_data.json';
+import githubLogo from '/Users/chartley/Documents/Projects/chartley-website/src/images/github-mark.svg';
 
 const iterate = (obj: Object) => {
-    const objectArray: Object[] = [];
-    Object.keys(obj).forEach(key => {
-        if (typeof obj[key] === 'object' && obj[key] !== null) {
-                objectArray.push(obj[key]);
-        };
-    })
-    return objectArray;
-}
+	const objectArray: Object[] = [];
+	Object.keys(obj).forEach((key) => {
+		if (typeof obj[key] === 'object' && obj[key] !== null) {
+			objectArray.push(obj[key]);
+		}
+	});
+	return objectArray;
+};
 
 const projectArray = iterate(projects_data);
 
+const listProjects = projectArray.map((project) => (
+	<div className='project'>
+		<div className='project-header'>
+			<h2>{project.title}</h2>
+			<div>
+                <img src={githubLogo}></img>
+            </div>
+		</div>
+		<div className='content-wrapper'>
+			<p>{project.description}</p>
+			<img src={project.image_src}></img>
+		</div>
+        <a href={project.live_url}>Check it out!</a>
+	</div>
+));
 
-const listProjects = projectArray.map(project =>
-    <div className="project">
-        <h2>{project.title}</h2>
-        <div>
-            <p>{project.description}</p>
-            <img src={project.image_src}></img>
-        </div>
-    </div>
-    )
+function Projects() {
+	return (
+		<div className='projects'>
+			<h1>Projects</h1>
+			<ul>{listProjects}</ul>
+		</div>
+	);
+}
 
-function Projects () {
-    return <div className="projects">
-        <h1>Projects</h1>
-        <ul>
-            {listProjects}
-        </ul>
-    </div>
-
-};
-
-export default Projects
+export default Projects;
